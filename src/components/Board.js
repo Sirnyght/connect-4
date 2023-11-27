68ada3bef30211ba046fe4f7a8f9246231bb8fe5
HUDE Dimitri <dimitri.hude@etu.univ-amu.fr>
2023-11-27 17:48:43 +0100

    Created Board component

---
+++ src/components/Board.js
@@ -0,0 +1,16 @@
+import Cell from "./Cell.js";
+
+export default function Board({ board, updateBoard, turn }) {
+    return (
+        <div id="board">
+            <h2>Board</h2>
+            <div id="board-grid">
+                {/* 42 cells */}
+                {board.map((cell, index) => {
+                    return <Cell key={index} updateBoard={updateBoard} index={index} turn={turn} />;
+                }
+                )}
+            </div>
+        </div>
+    );
+}
