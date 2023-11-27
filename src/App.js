eed97a0fd25e95b639b461dca992e4f3c68deda3
HUDE Dimitri <dimitri.hude@etu.univ-amu.fr>
2023-11-27 17:49:27 +0100

    Updated App.js to have the basic exoskeleton of a connect-4 application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Board from './components/Board.js';
 
 function App() {
+  // board from 0 to 42
+  const baseBoard = [0, 0, 0, 0, 0, 0, 0, 
+                 0, 0, 0, 0, 0, 0, 0, 
+                 0, 0, 0, 0, 0, 0, 0, 
+                 0, 0, 0, 0, 0, 0, 0, 
+                 0, 0, 0, 0, 0, 0, 0, 
+                 0, 0, 0, 0, 0, 0, 0];
+
+  const [turn, setTurn] = useState(1); // 1 or 2
+  const [board, setBoard] = useState(baseBoard);
+
+
+  function updateBoard(index, value) {
+    // update the board
+    const newBoard = board;
+    console.log(index);
+    console.log(newBoard[index]);
+    if (newBoard[index] === 0) {
+      if (turn === 1) {
+        setTurn(2);
+      }
+      else {
+        setTurn(1);
+      }
+      console.log("here");
+      newBoard[index] = value;
+    }
+    else {
+      console.log("Case already filled");
+      return;
+    }
+    setBoard(newBoard);
+    console.log(board); 
+    return newBoard;
+  }
+
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Connect 4</h1>
+      <div className="board">
+        <Board board={board} updateBoard={updateBoard} turn={turn} />
+      </div>
     </div>
   );
 }
