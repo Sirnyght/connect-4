835e4177d593bccfa9293f2089731bfcbe8ed310
HUDE Dimitri <dimitri.hude@etu.univ-amu.fr>
2023-11-29 15:52:09 +0100

    Adds .gitlab-ci.yml with unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { fireEvent } from '@testing-library/dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Connect 4/);
+  expect(titleElement).toBeInTheDocument();
 });
+
+test('renders board', () => {
+  render(<App />);
+  const boardElement = screen.getByText(/Board/);
+  expect(boardElement).toBeInTheDocument();
+});
+
+test('renders cells', () => {
+  render(<App />);
+  const cellElement = screen.getAllByText(/0/);
+  expect(cellElement).toHaveLength(42);
+});
+
+test('clicking on a cell updates the board', () => {
+  render(<App />);
+  const cellElement = screen.getAllByText(/0/);
+  act(() => {
+    fireEvent.click(cellElement[0]);
+  });
+  // check if the lowest cell in the first column has been updated
+  const updatedCellElement = cellElement[35];
+  expect(updatedCellElement).toHaveTextContent('1');
+});
+
