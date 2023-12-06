0015255edfb951a92b2e48def04320b378c7b119
HUDE Dimitri <dimitri.hude@etu.univ-amu.fr>
2023-12-06 18:47:42 +0100

    Please work

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';  
 import reportWebVitals from './reportWebVitals';
 
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(9000);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
