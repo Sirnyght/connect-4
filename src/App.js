6b73076132b48c8b894af1b8850ba0fa6fe8b85f
HUDE Dimitri <dimitri.hude@etu.univ-amu.fr>
2023-12-06 12:53:00 +0100

    Added reset button and deep copy of cells const to avoid errors

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { checkWin } from './utils/checkWin.js';
 
 function App() {
   const [turn, setTurn] = useState(1); // 1 or 2
-  const [board, setBoard] = useState(cells);
+  const [board, setBoard] = useState(JSON.parse(JSON.stringify(cells)));
   const [gameOver, setGameOver] = useState(false); // true or false
 
   function updateBoard(cell, value) {
@@ -16,17 +16,19 @@ function App() {
     // First, get cell column and row
     const col = cell.col;
     // Then, get the cells in that column
-    const columnCells = board.filter((c) => c.col === col);
+    const columnCells = board.filter((c) => c.col === col); 
     // For the first cell in that column starting from the end that has a value of 0, update it.
     // Skip this if there is no row above it
     const firstEmptyCell = columnCells.reverse().find((c) => c.value === 0);
+    
     if (firstEmptyCell) {
       firstEmptyCell.value = value;
       setBoard([...board]);
       setTurn(turn === 1 ? 2 : 1);
     }
+
     if (!firstEmptyCell) return;
-    
+
     const winner = checkWin(board, firstEmptyCell);
     if (winner === 1) {
       // Set game over to true
@@ -42,12 +44,22 @@ function App() {
       alert('Tie!');
     }
   }
+
+  function resetBoard() {
+    console.log('resetting board');
+    setBoard(JSON.parse(JSON.stringify(cells)));
+    setTurn(1);
+    setGameOver(false);
+  }
   
   return (
     <div className="app">
       <h1>Connect 4</h1>
       <div className="board">
         <Board board={board} updateBoard={updateBoard} turn={turn} />
+      </div>
+      <div className="buttons">
+        <button id="resetButton" onClick={resetBoard}>Reset</button>
       </div>
     </div>
   );
