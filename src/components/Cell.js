30108abc8acab4b222ec622f146e59f88b6b77ee
HUDE Dimitri <dimitri.hude@etu.univ-amu.fr>
2023-12-06 10:31:41 +0100

    Deleted useless old commented lines

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -6,14 +6,7 @@ export default function Cell({ updateBoard, cell, turn }) {
   }
 
   return (
-    // cell data-testid=cell- + index
 
-    // <div onClick={handleClick} className={info.position > 6 ? 'gamesquare' : player === 'player1' ? 'gamesquare top redmarker' : 'gamesquare top yellowmarker'}>
-    // {info.value ?
-    //     <div className={`testpiece_${info.position} gamesquare ${info.value}`} >
-    //         {info.winner ? <div className='win'></div> : null}
-    //     </div> : null}
-    // </div>
     <div onClick={handleClick} className="cell">
       {cell.value}
     </div>
