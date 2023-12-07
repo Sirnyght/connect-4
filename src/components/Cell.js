fe8179c283041a2d3cca4646fde586d2d23d886c
HUDE Dimitri <dimitri.hude@etu.univ-amu.fr>
2023-12-07 19:22:34 +0100

    Addition of new Token component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,3 +1,5 @@
+import Token from './Token.js';
+
 export default function Cell({ updateBoard, cell, turn }) {
   function handleClick() {
     // update the board
@@ -7,9 +9,7 @@ export default function Cell({ updateBoard, cell, turn }) {
   return (
     // add class player-turn where turn is turn props, test id is cell
     <div onClick={handleClick} className="cell" data-testid="cell">
-      {/* {cell.value} */}
+      {cell.value !== 0 ? <Token position={cell.index} player={cell.value} /> : null}
     </div>
-
-
   );
 }
