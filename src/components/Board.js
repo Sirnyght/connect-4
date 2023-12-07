3d3f7152e280c1b5555c71c7d11000f49322baef
HUDE Dimitri <dimitri.hude@etu.univ-amu.fr>
2023-12-07 11:33:41 +0100

    Added logo, updated application design

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,6 @@ import Cell from "./Cell.js";
 export default function Board({ board, updateBoard, turn }) {
     return (
         <div id="board">
-            <h2>Board</h2>
             <div id="board-grid">
                 {/* 42 cells */}
                     {board.map((cell, index) => {
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,12 +2,13 @@ export default function Cell({ updateBoard, cell, turn }) {
   function handleClick() {
     // update the board
     updateBoard(cell, turn);
+
   }
 
   return (
-
+    // add class player-turn where turn is turn props
     <div onClick={handleClick} className="cell">
-      {cell.value}
+      {/* {cell.value} */}
     </div>
 
 
