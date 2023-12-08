a8fa7793e57ebf2b9fe0a74691bd3abcd6d70217
HUDE Dimitri <dimitri.hude@etu.univ-amu.fr>
2023-12-08 15:08:23 +0100

    Separated div

--- src/components/Token.js
+++ src/components/Token.js
@@ -2,6 +2,7 @@ export default function Token({ position, player }) {
 
   return (
     // add class token_ position where position is position props and turn is  turn === 1 ? player-1 : player-2 
-    <div className={`token token_${position} ${player === 1 ? 'player-1' : 'player-2'}`} />
+    <div className={`token token_${position} ${player === 1 ? 'player-1' : 'player-2'}`}>
+    </div>
   );
 }
